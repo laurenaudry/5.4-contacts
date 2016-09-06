c111a0a2e70a9dc5438c17f6bc292990195a0b59
Lauren Johnson <laurenaudry1995@gmail.com>
2016-09-06 14:01:08 -0500

    fixed issues so contacts show up

---
+++ __tests__/contacts-test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import Contacts from '../lib/Contacts';
+
+jest.unmock('../lib/contacts');
+
+describe("Contacts", function(){
+
+  it("should show the contacts names", function(){
+    expect(Contacts.render()).toContain(`<h2></h2>`);
+  });
+});
